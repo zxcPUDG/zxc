c95923ec925f32f382fdf1ddb7c06ac5875c83a1
adel-gain-2007@yandex.ru <adel-gain-2007@yandex.ru>
2024-01-20 21:03:15 +0300

    new file

---
+++ gg2.py
@@ -0,0 +1,3 @@
+zarplata=6900
+pokupka=(((150*4+5*90)*70)/100)+140*2+385
+print(zarplata-pokupka)
---
+++ lesson.py
@@ -0,0 +1,109 @@
+# Hello=2
+# world=3
+# print(Hello,world,'Hello world')
+# str()   #'qefqefqfqfqfq'
+# int()   #2 4 6
+# float() #2.3
+# list() #['21','25','15']
+
+# list=['44','21','62']
+# list[0]=int(list[0])
+# list[2]=int(list[2])
+# print(list[0]+list[2], '+')
+# print(list[0]-list[2], '-')
+# print(list[0]*list[2], '*')
+# print(list[0]/list[2], '/') 
+
+# if 15>=20:
+# 	print('true')
+# else:
+# 	print('false')
+# print('do you like play on pudge???')
+# cart=[]
+# eggs=True
+# sweet=False
+# bread=True
+# if bread==True:
+# 	cart.append('bread')
+# #print(cart)
+# if eggs==True:
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+# if sweet==True:
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# print(cart)	
+
+# login=['shadow fiend' ,'pudge' ,'zxc']
+# password=['ezmid','my dick is big','igra proebana opyat(']
+# your_login=input('login:')
+# if your_login==login[0]:
+# 	your_password=input('password:')
+# 	if your_password==password[0]:
+# 		print('success')
+# 	else:
+# 		print('password incorrect')
+# else:
+# 	print('login incorrect')
+
+# if your_login==login[1]:
+# 	your_password=input('password:')
+# 	if your_password==password[1]:
+# 		print('success')
+# 	else:
+# 		print('password incorrect')
+# else:
+# 	print('login incorrect')
+
+# if your_login==login[2]:
+# 	your_password=input('password:')
+# 	if your_password==password[2]:
+# 		print('success')
+# 	else:
+# 		print('password incorrect')
+# else:
+# 	print('login incorrect')
+# 	#123adel
+# 2.1 практикум
+# week=['saturday','sanday']
+# week1=['monday','tuesday','wendsay','friday']
+# today=input('what day of the week is today?')
+# if today==week1[0]:
+# 	print('study study study')
+# elif today==week1[1]:
+# 	print('study study study')
+# elif today==week1[2]:
+# 	print('study study study')
+# elif today==week1[3]:
+# 	print('study study study')
+# elif today==week[0]:
+# 	print('party')
+# else:
+# 	print('have a rest')
+
+x=int(input('какая температура ?'))
+if x < 10 :
+	print('ПРОХЛАДНО')
+elif x < 30:
+	print('ТЕПЛО')
+elif x >= 30:
+	print('ЖАРКО')
+
+
+
+
+	
+
+	
+	
+
