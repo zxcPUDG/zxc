04ce898bc5405e438296e37d094e5eb6427fce28
zxcPUDG <156837039+zxcPUDG@users.noreply.github.com>
2024-01-24 12:21:05 +0300

    123
    123

---
+++ zxc-main/gg2.py
@@ -0,0 +1,18 @@
+# for x in range(10):
+#     print('You are welcome!')
+# i=int(input('введите число повторений: '))
+# for z in range(i):
+#     print('Silence is golden')
+# sr=int(input('сколько строк?: '))
+# for f in range(sr):
+#     print('00000')
+i=-1
+p=0
+for l in range(0,13):
+    i=i+2
+    p=p+2
+    print(i,'первый')
+    print(p, 'второй')
+print(p , 'расчет окончен')
+
+
